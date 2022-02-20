8617546edc5a001cf3f37cb55cb397505ecd0a65
Emma Shih <emma.shih@shopee.com>
2022-02-20 12:16:48 +0800

    install nodemon and practice response an object and status

--- server.js
+++ server.js
@@ -2,16 +2,32 @@ const http = require('http');
 
 const requestListener = (req, res) => {
   const headers = {
-    'content-Type': 'text/plain',
+    'Access-Control-Allow-Headers':
+      'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json',
   };
+
   // only user access to the homepage and only by "GET" method
   if (req.url == '/' && req.method == 'GET') {
     res.writeHeader(200, headers);
-    res.write('123');
+    // request can only recognize string
+    res.write(
+      JSON.stringify({
+        status: 'success',
+        data: [],
+      })
+    );
     res.end();
   } else {
     res.writeHeader(404, headers);
-    res.write('not found 404');
+    res.write(
+      JSON.stringify({
+        status: 'false',
+        message: '無此網站路由',
+      })
+    );
     res.end();
   }
 };
